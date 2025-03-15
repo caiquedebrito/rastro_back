811aca9a97e6eca1c7a9a1d72ecf381564e78dff
Caique <caiquedebritoo@gmail.com>
2025-03-15 11:11:00 -0300

    feat: implement authentication module with sign-in and sign-up endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
